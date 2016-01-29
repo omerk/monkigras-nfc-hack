4bf371b9d6766ec5bdbe4685214e8c610aeea4c1
Omer Kilic <omerkilic@gmail.com>
2016-01-29 01:56:11 +0000

    import nfc-reader

---
+++ nfc-reader/app/build/generated/source/buildConfig/debug/se/anyro/nfc_reader/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package se.anyro.nfc_reader;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "se.anyro.nfc_reader";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 13;
+  public static final String VERSION_NAME = "0.13";
+}
